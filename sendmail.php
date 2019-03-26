9c8d3b63ed52eb773aa4cdc33596aa4a64861b19
prasanna08cs <prasanna08cs@gmail.com>
2019-03-26 23:09:48 +0530

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+ ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "test@hostinger-tutorials.com";
+    $to = "prasanna08cs@gmail.com";
+    $subject = "Email from website";
+    $message = @$_POST['name']."-".@$_POST['email'];
+    $headers = "From:" . $from;
+    echo mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
+
+
+
+?>
